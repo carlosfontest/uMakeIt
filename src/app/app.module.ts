80d29d9ee5db59af70c94ed8e05a8770b9dea801
Carlos Fontes <fontessystems@gmail.com>
2018-10-05 22:39:52 -0400

    cambio de componentes en el inicio login/register, fix en responsive para celulares

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/inicio/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { RegisterComponent } from './components/inicio/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    InicioComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.scss']
+})
+export class InicioComponent implements OnInit {
+  login: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.login = true;
+  }
+
+}
