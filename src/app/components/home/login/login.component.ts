9ce5714573f472dd080e5df0bd43608c009e16c7
Rafael Quintero <rafaelq.sistemas@gmail.com>
2018-10-25 03:45:14 -0400

    se crea en el login el anchor para abrir el modal y se implementa

--- src/app/components/home/login/login.component.ts
+++ src/app/components/home/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { RecoverPasswordModalComponent } from '../../shared/modals/recover-password-modal/recover-password-modal.component';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +12,15 @@ import { FlashMessagesService } from 'angular2-flash-messages';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  bsModalRef: BsModalRef;
   email: string;
   password: string;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private flashMessage: FlashMessagesService
+    private flashMessage: FlashMessagesService,
+    private modalService: BsModalService
   ) { }
 
   ngOnInit() {
@@ -35,6 +40,13 @@ export class LoginComponent implements OnInit {
             cssClass: 'alert-danger', timeout: 2000
           });
         });
+  }
+
+  recoverPasswordModal(){
+    const initialState = {
+      title: 'Recover your Password'
+    };
+    this.bsModalRef = this.modalService.show(RecoverPasswordModalComponent, {initialState});
   }
 
 }
