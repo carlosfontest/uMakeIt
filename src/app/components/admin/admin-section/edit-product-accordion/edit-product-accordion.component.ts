87df21bbd88438c71414c33f872862b52f81d31b
Carlos Fontes <fontessystems@gmail.com>
2018-11-12 20:34:30 -0400

    se pueden ver platos editables en editar producto, en la vista de administrador

--- src/app/components/admin/admin-section/edit-product-accordion/edit-product-accordion.component.ts
+++ src/app/components/admin/admin-section/edit-product-accordion/edit-product-accordion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from 'src/app/models/Dish';
 import { DishService } from 'src/app/services/dish.service';
+import { SideDish } from 'src/app/models/SideDish';
+import { SideDishService } from 'src/app/services/side-dish.service';
 
 @Component({
   selector: 'app-edit-product-accordion',
@@ -9,14 +11,33 @@ import { DishService } from 'src/app/services/dish.service';
 })
 export class EditProductAccordionComponent implements OnInit {
   allDishes: Dish[];
+  sideDishes: SideDish[];
 
-  constructor(private dishService: DishService) { }
+  constructor(
+    private dishService: DishService,
+    private sideDishService: SideDishService
+    ) { }
 
   ngOnInit() {
     // Le pedimos a Firestore los platos
     this.dishService.getDishes().subscribe(data => {
       this.allDishes = data;
     });
+
+    // Cargamos todos los Side-Dishes
+    this.sideDishes = [];
+
+    this.sideDishService.getSideDishes().subscribe(data => {
+      this.sideDishes = data;
+    });
+  }
+
+  buscarSideDish(id: string): string {
+    return this.sideDishes.find(sideDish => sideDish.id === id).thumbnailPlatoArriba;
+  }
+  
+  buscarSideDishDoble(id: string): string {
+    return this.sideDishes.find(sideDish => sideDish.id === id).thumbnailPlatoDoble;
   }
 
 }
