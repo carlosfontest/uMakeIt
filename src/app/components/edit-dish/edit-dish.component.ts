5e59c6e1d37de38e770f53ad5ed81efce2a7c31b
Carlos Fontes <fontessystems@gmail.com>
2018-11-12 23:25:22 -0400

    implementación terminada de edit-dish, sin agregar al carrito

--- src/app/components/edit-dish/edit-dish.component.ts
+++ src/app/components/edit-dish/edit-dish.component.ts
@@ -13,6 +13,7 @@ import { DishService } from 'src/app/services/dish.service';
 })
 export class EditDishComponent implements OnInit {
   dish: Dish;
+  dishSideDishes: SideDish[];
   sideDishes: SideDish[];
   cantSideDishSelected: number;
   @ViewChild('modal') modal: TemplateRef<any>;
@@ -21,40 +22,91 @@ export class EditDishComponent implements OnInit {
   loaded: boolean;
 
   constructor(private sideDishService: SideDishService,
-    private ar: ActivatedRoute, 
-    private ds: DishService, 
+    private ar: ActivatedRoute,
+    private dishService: DishService,
     private router: Router,
     private modalService: BsModalService
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.loaded = false;
+    this.dishSideDishes = [];
 
-    this.ds.getDishById(this.ar.snapshot.queryParams.id).subscribe(dish => {
+    this.dishService.getDishById(this.ar.snapshot.queryParams.id).subscribe(dish => {
+      this.loaded = false;
       this.dish = dish;
       if (dish) {
         this.sideDishService.getSideDishes().subscribe(data => {
           this.sideDishes = data;
-          this.loaded = true;
-          this.abrirModalInstrucciones();
+          // this.abrirModalInstrucciones();
+          // Obtenemos los Side-Dishes del plato en caso de que sea editable
+          if (this.dish.sideDishes) {
+            // Obtenemos el Side-Dish uno
+            this.sideDishService.getSideDishById(this.dish.sideDishes[0]).subscribe(data2 => {
+              this.dishSideDishes[0] = data2;
+              // Obtenemos el Side-Dish dos
+              this.sideDishService.getSideDishById(this.dish.sideDishes[1]).subscribe(data3 => {
+                this.dishSideDishes[1] = data3;
+                this.cantSideDishSelected = this.dishSideDishes.length;
+                this.loaded = true;
+              });
+            });
+          }
         });
       } else {
         this.router.navigate(['**']);
       }
-    });
-    
-    this.cantSideDishSelected = 0;
 
-    
+    });
   }
 
-  onChangeCant(n: number) {
+  onChangeCant(n: any) {
     // Ingredientes seleccionados en total
-    this.cantSideDishSelected = this.cantSideDishSelected + n;
+    this.cantSideDishSelected = this.cantSideDishSelected + n.num;
+
+    // Si se agrega
+    if (n.num > 0) {
+      if (this.dishSideDishes[0] == null) {
+        // Poner en la primera posición
+        this.dishSideDishes[0] = n.sideDish;
+        console.log(n.sideDish.id);
+      } else {
+        // Poner en la segunda posición
+        this.dishSideDishes[1] = n.sideDish;
+        console.log(n.sideDish.id);
+      }
+    }
+
+    // Si se quita
+    if (n.num < 0) {
+      for (let i = 0; i < this.dishSideDishes.length; i++) {
+        if (this.dishSideDishes[i]) {
+          if (this.dishSideDishes[i].id === n.sideDish.id) {
+            this.dishSideDishes[i] = null;
+            console.log(this.dishSideDishes);
+            return;
+          }
+        }
+      }
+    }
   }
 
   abrirModalInstrucciones() {
     this.bsModalRef = this.modalService.show(this.modal);
+  }
+
+  obtenerCantidadInicial(sideDish: SideDish) {
+    let cant = 0;
+
+    for (const sideDishItem of this.dishSideDishes) {
+      if (sideDishItem) {
+        if (sideDishItem.id === sideDish.id) {
+          cant++;
+        }
+      }
+    }
+
+    return cant;
   }
 
 }
--- src/app/components/edit-dish/sticky-dish-view/sticky-dish-view.component.ts
+++ src/app/components/edit-dish/sticky-dish-view/sticky-dish-view.component.ts
@@ -10,23 +10,12 @@ import { SideDishService } from 'src/app/services/side-dish.service';
 })
 export class StickyDishViewComponent implements OnInit {
   @Input() dish: Dish;
-  sideDishes: SideDish[];
+  @Input() sideDishes: SideDish[];
 
   constructor(private sideDishService: SideDishService) { }
 
   ngOnInit() {
-    this.sideDishes = [];
-    // Obtenemos los Side-Dishes del plato en caso de que sea editable
-    if (this.dish.sideDishes) {
-      // Obtenemos el Side-Dish uno
-      this.sideDishService.getSideDishById(this.dish.sideDishes[0]).subscribe(data => {
-        this.sideDishes[0] = data;
-      });
-      // Obtenemos el Side-Dish dos
-      this.sideDishService.getSideDishById(this.dish.sideDishes[1]).subscribe(data => {
-        this.sideDishes[1] = data;
-      });
-    }
+    console.log(this.sideDishes);
   }
   
 
