b803801259c50f7d28e461979bc3902d9d224a2c
Carlos Fontes <fontessystems@gmail.com>
2018-11-25 15:53:06 -0400

    se termina el visual del bill-modal

--- src/app/components/shared/modals/bill-modal/bill-modal.component.ts
+++ src/app/components/shared/modals/bill-modal/bill-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
+import { OrderDish } from 'src/app/models/OrderDish';
 
 @Component({
   selector: 'app-bill-modal',
@@ -7,12 +8,21 @@ import { BsModalRef } from 'ngx-bootstrap';
   styleUrls: ['./bill-modal.component.scss']
 })
 export class BillModalComponent implements OnInit {
+  cart: OrderDish[];
 
   constructor(
     public bsModalRef: BsModalRef
   ) { }
 
   ngOnInit() {
+  }
+
+  get precioTotal() {
+    let price = 0;
+    for (let i = 0; i < this.cart.length; i++) {
+      price += this.cart[i].dish.price * this.cart[i].quantity;
+    }
+    return price;
   }
 
 }
