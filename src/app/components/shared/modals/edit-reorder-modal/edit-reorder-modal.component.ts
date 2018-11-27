9346831436477d149afea681d12ecaea1016b913
Carlos Fontes <fontessystems@gmail.com>
2018-11-27 03:06:13 -0400

    se termina la implementación del edit-reorder

--- src/app/components/shared/modals/edit-reorder-modal/edit-reorder-modal.component.ts
+++ src/app/components/shared/modals/edit-reorder-modal/edit-reorder-modal.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderDish } from 'src/app/models/OrderDish';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { SortablejsOptions } from 'angular-sortablejs';
+import { BillModalComponent } from '../bill-modal/bill-modal.component';
+import { SnotifyService } from 'ng-snotify';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-reorder-modal',
@@ -9,19 +13,53 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 })
 export class EditReorderModalComponent implements OnInit {
   cart: OrderDish[];
-  dishesInOldCart: OrderDish[] = this.cart;
-  dishesInNewCart: OrderDish[] = this.cart;
+  dishesInOldCart: OrderDish[];
+  dishesInNewCart: OrderDish[];
+  normalOptions: SortablejsOptions = {
+    group: 'normal-group'
+  };
 
   constructor(
-    public bsModalRef: BsModalRef
+    public bsModalRef: BsModalRef,
+    private modalService: BsModalService,
+    private snotifyService: SnotifyService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     // Inicializamos los dos lados del sortable
+    this.dishesInOldCart = this.cart;
+    this.dishesInNewCart = [];
   }
 
   proceed() {
+    if (this.dishesInNewCart.length !== 0) {
+      console.log(this.dishesInNewCart);
+      const initialState = {
+        cart: this.dishesInNewCart,
+        price: this.calculatePrice()
+      };
+      this.bsModalRef.hide();
+      this.bsModalRef = this.modalService.show(BillModalComponent, { initialState });
+      this.router.navigate(['/']);
+    } else {
+      this.snotifyService.error('You have to place some article in the new order', 'Re-order', {
+        timeout: 2500,
+        showProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        position: 'leftBottom'
+      });
+    }
+  }
 
+  calculatePrice(): number {
+    let price = 0;
+    for (let i = 0; i < this.dishesInNewCart.length; i++) {
+      price += this.dishesInNewCart[i].quantity * this.dishesInNewCart[i].dish.price;
+    }
+    console.log(price);
+    return price;
   }
 
 }
