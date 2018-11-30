374ef96d81d646637c3d884e61af6ddfb60ad84f
Carlos Fontes <fontessystems@gmail.com>
2018-11-30 03:08:47 -0300

    arreglo en la vista de ordenes pendientes y arreglo de bug al poner delivered una orden

--- src/app/components/admin/admin-section/pending-orders-accordion/pending-orders-accordion.component.ts
+++ src/app/components/admin/admin-section/pending-orders-accordion/pending-orders-accordion.component.ts
@@ -5,6 +5,7 @@ import { UserService } from 'src/app/services/user.service';
 import { take } from 'rxjs/operators';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { ProductsModalComponent } from 'src/app/components/shared/modals/products-modal/products-modal.component';
+import { MomentDatesService } from 'src/app/services/moment-dates.service';
 
 @Component({
   selector: 'app-pending-orders-accordion',
@@ -21,7 +22,8 @@ export class PendingOrdersAccordionComponent implements OnInit, OnChanges {
     private ordersService: OrderService,
     public bsModalRef: BsModalRef,
     private modalService: BsModalService,
-    private userService: UserService
+    private userService: UserService,
+    private momentService: MomentDatesService
   ) { }
 
   ngOnInit() {
@@ -29,9 +31,11 @@ export class PendingOrdersAccordionComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
     const orders: SimpleChange = changes.pendingOrders;
 
     if (orders.currentValue && orders.currentValue.length !== 0) {
+      this.names = [];
       let index = 0;
       for (const order of orders.currentValue) {
         this.userService.getUserById(order.uid).pipe(take(1)).subscribe(user => {
@@ -43,7 +47,6 @@ export class PendingOrdersAccordionComponent implements OnInit, OnChanges {
         });
       }
     }
-
   }
 
   mostrarOrdenModal(orden: Order) {
@@ -62,6 +65,10 @@ export class PendingOrdersAccordionComponent implements OnInit, OnChanges {
     orden.delivered = !orden.delivered;
     this.pendingOrders.slice(index, 1);
     this.ordersService.updateOrder(orden);
+  }
+
+  getDate(date: Date) {
+    return this.momentService.formatDateShort(date);
   }
 
 
